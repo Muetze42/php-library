feea548b934ecca938ea77d949093602741e5c39
Norman Huth <norman@huth.it>
2024-04-13 23:31:56 +0200

    feat: add delete directory method

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -7,6 +7,7 @@ use NormanHuth\Library\Core\Filesystem as Instance;
 /**
  * @method allDirectories(string|array $paths, bool $hidden = true): array
  * @method allFiles(string|array $paths, string $pattern = '*', bool $hidden = true): array
+ * @method deleteDirectory(string $path): bool
  */
 class Filesystem
 {
