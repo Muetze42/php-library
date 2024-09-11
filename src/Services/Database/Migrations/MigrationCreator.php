6d4a29efc3be2aedae41a8df152d717463ff6701
Norman Huth <norman@huth.it>
2024-09-11 19:48:57 +0200

    refactor: optimize migration creator ignore zero migration files

--- src/Services/Database/Migrations/MigrationCreator.php
+++ src/Services/Database/Migrations/MigrationCreator.php
@@ -8,6 +8,11 @@ use Illuminate\Filesystem\Filesystem;
 class MigrationCreator extends Creator
 {
     /**
+     * @var string
+     */
+    protected string $formattedToday;
+
+    /**
      * Create a new migration creator instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem  $files
@@ -42,8 +47,18 @@ class MigrationCreator extends Creator
     {
         $files = glob(database_path('migrations/*'));
         $today = now()->format('Y_m_d_');
-        $files = array_filter($files, fn (string $file) => str_starts_with(basename($file), $today));
+        $files = array_filter($files, fn (string $file) => static::isFileFromToday($file, $today));
 
         return $today . str_pad((count($files) + 1) * 10, 6, '0', STR_PAD_LEFT);
+    }
+
+    /**
+     * @param  string  $file
+     * @param  string  $today
+     * @return bool
+     */
+    protected static function isFileFromToday(string $file, string $today): bool
+    {
+        return str_starts_with(basename($file), $today) && ! str_starts_with(basename($file), $today . '00000');
     }
 }
