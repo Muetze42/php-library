7b59a7c226ea1927072a2c6d5f953651103e1441
Norman Huth <norman@huth.it>
2024-09-18 13:27:59 +0200

    refactor: remove array property

--- src/Support/Cast.php
+++ src/Support/Cast.php
@@ -90,8 +90,6 @@ class Cast
     /**
      * Try to get the value as array.
      *
-     * @return array
-     *
      * @throws\NormanHuth\Library\Exceptions\CastException
      */
     public function toArray(): array
