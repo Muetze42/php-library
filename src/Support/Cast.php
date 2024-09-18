c25f64f5b8e99c02165720544067ba2353bf0120
Norman Huth <norman@huth.it>
2024-09-18 13:10:00 +0200

    feat: add array case to cast class

--- src/Support/Cast.php
+++ src/Support/Cast.php
@@ -2,13 +2,16 @@
 
 namespace NormanHuth\Library\Support;
 
+use Exception;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Str;
 use Illuminate\Support\Stringable as IlluminateStringable;
 use Illuminate\Support\Traits\Macroable;
 use JsonSerializable;
 use NormanHuth\Library\Exceptions\CastException;
 use Stringable;
+use Throwable;
 
 class Cast
 {
@@ -39,33 +42,90 @@ class Cast
     {
         $value = $this->value;
 
-        if (is_null($value)) {
-            return '';
+        if (is_string($value) || is_null($value)) {
+            return (string) $value;
         }
+
         if ($value instanceof Jsonable || is_object($value) && method_exists($value, 'toJson')) {
-            return $value->toJson();
+            return (new self($value->toJson()))->toString($separator);
         }
+
         if ($value instanceof IlluminateStringable || is_object($value) && method_exists($value, 'toString')) {
-            return $value->toString();
+            return (new self($value->toString()))->toString($separator);
         }
+
         if ($value instanceof JsonSerializable) {
             return (new self($value->jsonSerialize()))->toString($separator);
         }
+
         if ($value instanceof Arrayable || is_object($value) && method_exists($value, 'toArray')) {
-            $value = $value->toArray();
-        }
-        if (is_array($value)) {
+            try {
+                $value = (new self($value->toArray()))->toArray();
+            } catch (Exception | Throwable) {
+                throw new CastException('string');
+            }
+
             return implode($separator, $value);
         }
+
         if ($value instanceof Stringable || is_object($value) && method_exists($value, '__toString')) {
-            return $value->__toString();
+            try {
+                $value = (new self($value->__toString()))->toString();
+            } catch (Exception | Throwable) {
+                throw new CastException('string');
+            }
         }
+
         if (is_object($value)) {
-            return serialize($value);
+            $value = serialize($value);
         }
 
         if (! is_string($value)) {
             throw new CastException('string');
+        }
+
+        return $value;
+    }
+
+    /**
+     * Try to get the value as array.
+     *
+     * @return array<int, string>
+     *
+     * @throws\NormanHuth\Library\Exceptions\CastException
+     */
+    public function toArray(): array
+    {
+        $value = $this->value;
+
+        if (is_array($value)) {
+            return $value;
+        }
+
+        if (is_string($value)) {
+            if (Str::isJson($value)) {
+                $value = json_decode($value, true);
+
+                if (! is_array($value)) {
+                    throw new CastException('array');
+                }
+            }
+
+            if (is_string($value)) {
+                return (new self(preg_split('/\r\n|\n|\r/', $value)))->toArray();
+            }
+        }
+
+        if ($value instanceof Arrayable || is_object($value) && method_exists($value, 'toArray')) {
+            return (new self($value->toArray()))->toArray();
+        }
+
+        if ($value instanceof Jsonable || is_object($value) && method_exists($value, 'toJson')) {
+            return (new self($value->toJson()))->toArray();
+        }
+
+        if (! is_array($value)) {
+            throw new CastException('array');
         }
 
         return $value;
