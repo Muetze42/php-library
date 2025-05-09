d28cfdba84ad9e265acbe645e73dbd6f77f8e4af
Norman Huth <norman@huth.it>
2025-05-09 23:06:10 +0200

    feat(make-command): add method for command prefix injection

--- src/Commands/Development/ConsoleMakeCommand.php
+++ src/Commands/Development/ConsoleMakeCommand.php
@@ -5,6 +5,7 @@ namespace NormanHuth\Library\Commands\Development;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Foundation\Console\ConsoleMakeCommand as Command;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(name: 'make:command')]
@@ -22,8 +23,8 @@ class ConsoleMakeCommand extends Command
 
         $command = $this->option('command');
 
-        if (! $command) {
-            $command = Str::of($this->rootNamespace())->classBasename()->lower()->value();
+        if (empty($command) || ! is_string($command)) {
+            $command = $this->getSignaturePrefix();
 
             if (str_ends_with($name, 'Command') && strlen($name) > 7) {
                 $name = substr($name, 0, -7);
@@ -31,13 +32,20 @@ class ConsoleMakeCommand extends Command
 
             $defaultNamespace = str_replace('\\\\', '\\', $this->getDefaultNamespace($this->rootNamespace()));
             $commandClass = trim(str_replace($defaultNamespace, '', $name), '\\');
-            $levels = explode('\\', $commandClass);
 
-            foreach ($levels as $level) {
-                $command .= ':' . Str::kebab($level);
-            }
+            $levels = explode('\\', $commandClass);
+            $levels = array_map(fn (string $level) => Str::kebab($level), $levels);
+            $command .= ':' . implode(':', $levels);
         }
 
         return str_replace(['dummy:command', '{{ command }}'], $command, $stub);
+    }
+
+    /**
+     * Get the prefix for the name and signature of the console command.
+     */
+    protected function getSignaturePrefix(): string
+    {
+        return Str::of($this->rootNamespace())->classBasename()->lower()->value();
     }
 }
